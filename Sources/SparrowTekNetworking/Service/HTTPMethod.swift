effa5b37c781161df1a7d54f8450358c56f5836a
Thomas Rademaker <Thomas@SparrowTek.com>
2020-07-01 20:19:45 -0400

    Change HTTPMethod to struct from enum

--- Sources/SparrowTekNetworking/Service/HTTPMethod.swift
+++ Sources/SparrowTekNetworking/Service/HTTPMethod.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2019 SparrowTek. All rights reserved.
 //
 
-public enum HTTPMethod : String {
-    case get = "GET"
-    case post = "POST"
-    case put = "PUT"
-    case patch = "PATCH"
-    case delete = "DELETE"
+public struct HTTPMethod: Hashable {
+    public static let get = HTTPMethod(rawValue: "GET")
+    public static let post = HTTPMethod(rawValue: "POST")
+    public static let put = HTTPMethod(rawValue: "PUT")
+    public static let patch = HTTPMethod(rawValue: "PATCH")
+    public static let delete = HTTPMethod(rawValue: "DELETE")
+
+    public let rawValue: String
 }
